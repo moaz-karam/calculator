01d8c3d8ba198d6cfe4f33cdf3f9d4da95a1b729
movzkm <movzkm@gmail.com>
2025-09-07 12:51:21 +0300

    completed calculator functionality

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 
-const opAdd = 1;
-const opSub = 2;
-const opMul = 3;
-const opDiv = 4;
+const opAdd = "add";
+const opSub = "sub";
+const opMul = "mul";
+const opDiv = "div";
 
-let n1 = 0;
-let n2 = 0;
+const n1 = 0;
+const n2 = 1;
 
+let number1 = "0";
+let number2 = "0";
+
+let numbers = [number1, number2];
 let currentN = 0;
 
-let screenString = 0;
+let operation = "";
+
+let floating = false;
 
 function add(a, b) {
     return a + b;
 }
 function sub(a, b) {
-    return a - b;af
+    return a - b;
 }
 function mul(a, b) {
     return a * b;
@@ -27,26 +33,108 @@ function div(a, b) {
 function operate(op, a, b) {
     switch (op) {
         case opAdd:
-            add(a, b);
-            break;
+            return add(a, b);
         case opSub:
-            sub(a, b);
-            break;
+            return sub(a, b);
         case opMul:
-            mul(a, b);
-            break;
+            return mul(a, b);
         case opDiv:
-            div(a, b);
-            break;
+            return div(a, b);
     }
 }
+
+function addToScreen(item) {
+    screen.textContent += item;
+}
+
+function putToScreen(item) {
+    screen.textContent = item;
+}
+
+function insert(item) {
+    numbers[currentN] += item;
+}
+function setCurrentN(x) {
+    currentN = x;
+}
+
 
 const screen = document.querySelector("#screen");
 
 const numsClass = document.querySelectorAll(".nums");
-
 numsClass.forEach(item => {
+
     item.addEventListener("click", () => {
-        screen.textContent += item.textContent;
+
+        
+        if (screen.textContent == "0") {
+            putToScreen(item.textContent);
+        }
+        else {
+            addToScreen(item.textContent);
+        }
+        insert(item.textContent);
     })
+})
+
+const opsClass = document.querySelectorAll(".ops");
+opsClass.forEach(item => {
+
+    item.addEventListener("click", () => {
+
+        if (currentN == n1) {
+            setCurrentN(n2);
+        }
+        else {
+            numbers[n1] = operate(operation, parseFloat(numbers[n1])
+                                            , parseFloat(numbers[n2])) + "";
+            putToScreen(numbers[n1]);
+            numbers[n2] = "0";
+        }
+        addToScreen(item.textContent);
+        operation = item.getAttribute("id");
+        floating = false;
+    })
+})
+
+const buttonEqual = document.querySelector("#equal");
+buttonEqual.addEventListener("click", () => {
+
+    let temp = operate(operation, parseFloat(numbers[n1]) , parseFloat(numbers[n2]));
+
+    numbers[n1] = parseFloat(temp.toFixed(4)) + "";
+    putToScreen(numbers[n1]);
+    numbers[n2] = "0";
+    setCurrentN(n1);
+    floating = false;
+})
+
+const buttonAC = document.querySelector("#AC");
+buttonAC.addEventListener("click", () => {
+    setCurrentN(n1);
+    numbers[n1] = "";
+    numbers[n2] = "";
+
+    putToScreen("0");
+    operation = "";
+})
+
+const buttonDot = document.querySelector("#dot");
+buttonDot.addEventListener("click", () => {
+    
+    let temp = Math.floor(parseFloat(numbers[currentN]));
+
+    if (!floating && temp == numbers[currentN]) {
+        floating = true;
+        addToScreen('.');
+        numbers[currentN] += '.';
+    }
+})
+
+const buttonSign = document.querySelector("#sign");
+buttonSign.addEventListener("click", () => {
+    if (currentN == n1) {
+        numbers[n1] = -parseFloat(numbers[n1]) + "";
+        putToScreen(numbers[n1]);
+    }
 })
